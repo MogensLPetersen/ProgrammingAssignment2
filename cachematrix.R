9ef9c39d04617f62c21eacee19948fd7c1048661
Mogens L Petersen <mogenslpetersen@gmail.com>
2015-08-23 23:43:34 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
+## Some atempt to make assignment
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
+## This function make of data stored in enviroment.
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -9,7 +11,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## This function inverts a matrix's value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
