86a180564986386e898ac82d3616067b682a81e4
Mogens L Petersen <mogenslpetersen@gmail.com>
2015-08-23 23:31:55 +0200

    Create cachematrix.r

---
+++ cachematrix.r
@@ -0,0 +1,18 @@
+## Some attempt to make assignment 2
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+## This function make of data stored in enviroment.
+
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+
+
+## Write a short comment describing this function
+## This function inverts a matrix's value
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
